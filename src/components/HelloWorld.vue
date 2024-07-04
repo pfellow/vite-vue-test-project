3e1c1672e6fba0e0bbcc22831ca4905e37ceb7ca
pfellow <pfellow@gmail.com>
2024-07-04 22:39:28 +1200

    extend testing capabilities

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -10,7 +10,7 @@ const count = ref(0);
   <h1>{{ msg }}</h1>
 
   <div class="card">
-    <button data-test="counter-button" type="button" @click="count++">
+    <button data-testId="counter-button" type="button" @click="count++">
       count is {{ count }}
     </button>
     <p>
